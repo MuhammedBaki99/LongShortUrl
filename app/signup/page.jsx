2d42551c14b86f1454f60c6b84ec199f4021d2aa
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-10-08 17:58:53 +0300

    kayıt olma tasarımı oluşturuldu

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,13 +1,26 @@
-import { signUp } from "@/actions/auth"; 
-export default function SignUPPage(){ 
+import { signUp } from "@/actions/auth";
+import "./signup.css"
 
-    return(
-        <form action={signUp} >
-            <input type="text" name="firstName" placeholder="Adınız" /> <br />
-            <input type="text" name="lastName" placeholder="Soyadınız" /> <br />
-            <input type="email" name="email" placeholder="E-posta Adresiniz" /> <br />
-            <input type="password" name="password" placeholder="********" />
-            <button>Kayıt Ol</button>
-        </form>
+export default function SignUPPage() {
+
+    return (
+        <div className="signupCont">
+            <h1>Kayıt Ol</h1>
+            <form action={signUp} >
+                <label htmlFor="firstName">Adınız:
+                    <input type="text" name="firstName" placeholder="Adınız" />
+                </label>
+                <label htmlFor="lastName">Soyadınız:
+                    <input type="text" name="lastName" placeholder="Soyadınız" />
+                </label>
+                <label htmlFor="email">E-posta:
+                    <input type="email" name="email" placeholder="E-posta Adresiniz" />
+                </label>
+                <label htmlFor="password">Şifre
+                    <input type="password" name="password" placeholder="********" />
+                </label>
+                <button>Kayıt Ol</button>
+            </form>
+        </div>
     )
 }

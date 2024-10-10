64a4516204e924f76e0a8e2673db25b0ae3e3863
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-10-10 12:06:11 +0300

    signup tasarımının responsive eklendi

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,26 +1,32 @@
 import { signUp } from "@/actions/auth";
 import "./signup.css"
+import Image from "next/image";
 
 export default function SignUPPage() {
 
     return (
         <div className="signupCont">
-            <h1>Kayıt Ol</h1>
-            <form action={signUp} >
-                <label htmlFor="firstName">Adınız:
-                    <input type="text" name="firstName" placeholder="Adınız" />
-                </label>
-                <label htmlFor="lastName">Soyadınız:
-                    <input type="text" name="lastName" placeholder="Soyadınız" />
-                </label>
-                <label htmlFor="email">E-posta:
-                    <input type="email" name="email" placeholder="E-posta Adresiniz" />
-                </label>
-                <label htmlFor="password">Şifre
-                    <input type="password" name="password" placeholder="********" />
-                </label>
-                <button>Kayıt Ol</button>
-            </form>
+            <div className="signup">
+                <h1>Kayıt Ol</h1>
+                <form action={signUp} >
+                    <label htmlFor="firstName">Adınız:
+                        <input type="text" name="firstName" placeholder="Adınız" />
+                    </label>
+                    <label htmlFor="lastName">Soyadınız:
+                        <input type="text" name="lastName" placeholder="Soyadınız" />
+                    </label>
+                    <label htmlFor="email">E-posta:
+                        <input type="email" name="email" placeholder="E-posta Adresiniz" />
+                    </label>
+                    <label htmlFor="password">Şifre
+                        <input type="password" name="password" placeholder="********" />
+                    </label>
+                    <button>Kayıt Ol</button>
+                </form>
+            </div>
+            <div className="signupImg">
+                <Image src={"/image/hero.png"} width={500} height={500} />
+            </div>
         </div>
     )
 }
